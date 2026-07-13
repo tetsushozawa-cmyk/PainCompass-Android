d811f68fa80873d43250c6aabf91b2c4f77c8b19
tetsushozawa-cmyk <tetsushozawa@gmail.com>
2026-07-13 16:46:18 +0900

    公開前version1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.painrecord001"
+    namespace = "com.tetsushozawa.paincompass"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.painrecord001"
+        applicationId = "com.tetsushozawa.paincompass"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
